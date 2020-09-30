3ebb26f1271e25d0b7be26d06dc0b112e86737b7
hemie143 <hemie143@gmail.com>
2020-09-30 16:39:33 +0200

    Stage 2 ongoing

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,8 +1,14 @@
 from django.views import View
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         html = '<h2>Welcome to the Hypercar Service!</h2>'
         return HttpResponse(html)
+
+
+class MenuView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tickets/menu.html')
